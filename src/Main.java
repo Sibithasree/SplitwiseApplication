0951a4b067d328c2fbbf674d7f4caf29f6ab3cd9
Sibima <sibithasree.25bscaids@kprcas.ac.in>
2026-08-10 09:47:16 +0530

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    void greet() {
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.greet();
+    }
+}
