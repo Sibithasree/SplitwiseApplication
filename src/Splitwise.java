a158ae40e19cec6f22bd11e7e11de4c1bc7bec8f
Sibima <sibithasree.25bscaids@kprcas.ac.in>
2026-08-20 20:29:40 +0530

    5th commit

--- src/Splitwise.java
+++ src/Splitwise.java
@@ -1,17 +1,15 @@
+import java.util.Scanner;
 public class Splitwise {
-    void display()
-    {
-        System.out.println("Welcome to Splitwise!!");
-    }
-    public static void main(String[] args){
-        Splitwise obj=new Splitwise();
-        obj.display();
-        public static void main(String args[]){
-            System.out.println("===Splitwise Console Backend===");
-            System.out.println(" ");
-            final String expenseDescription="Dinner";
-            final double totalAmount=1200.0;
-            final int number_of_people=4;
+    public static void main(String args[]){
+        System.out.println("===Splitwise Console Backend===");
+        System.out.println(" ");
+        final String expenseDescription="Dinner";
+        final double totalAmount=1200.0;
+        final int number_of_people=4;
+        public static void main(String args[]) {
+            final String expenseDescription = "Dinner";
+            final double totalAmount = 1200.0;
+            final int number_of_people = 4;
             double individualShare;
             individualShare=totalAmount/number_of_people;
             System.out.println("Expense Description:"+expenseDescription);
@@ -20,5 +18,47 @@ public class Splitwise {
             System.out.println("Each Person should pay:"+individualShare);
             System.out.println(" ");
             System.out.println("Feature status:Basic calculation completed.");
+            individualShare = totalAmount / number_of_people;
+            final int MENU_ADD_EXPENSE =1;
+            final int MENU_VIEW_EXPENSE =2;
+            final int MENU_EXIT =3;
+            Scanner scanner = new Scanner(System.in);
+            boolean running = true;
+            while (running) {
+                System.out.println("===Splitwise Cinsole Backend===");
+                System.out.println();
+                System.out.println("1. Add Expense");
+                System.out.println("2. View Expense");
+                System.out.println("3. Exit");
+                System.out.print("Enter choice:");
+                int choice;
+                choice=scanner.nextInt();
+                System.out.println();
+                switch(choice){
+                    case MENU_ADD_EXPENSE->{
+                        System.out.println("Recording Expense(Sample data)...");
+                        System.out.println("Expense Saved:"+expenseDescription+"|amount="+totalAmount +"|people="+number_of_people);
+                        System.out.println();
+                    }
+                    case MENU_VIEW_EXPENSE->{
+                        System.out.println("---Expense summary---");
+                        System.out.println("Description:"+expenseDescription);
+                        System.out.println("Total Amount:"+totalAmount);
+                        System.out.println("Number of People:"+number_of_people);
+                        System.out.println("Individual Share:"+individualShare);
+                        System.out.println();
+                    }
+                    case MENU_EXIT->{
+                        running=false;
+                    }
+                    default ->{
+                        System.out.println("Invalid choice.Please enter 1 , 2 , 3.");
+                        System.out.println();
+                    }
+                }
+
+            }
+            System.out.println("GoodBye! Thank You for using Splitwise Console Backend.");
+            scanner.close();
         }
     }
