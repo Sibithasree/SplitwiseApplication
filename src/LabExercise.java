03016a456147b587e4e9fe4889866cb2756ffdb9
Sibima <sibithasree.25bscaids@kprcas.ac.in>
2026-08-20 20:25:57 +0530

    3rd commit

---
+++ src/LabExercise.java
@@ -0,0 +1,16 @@
+public class LabExercise {
+    public static void main(String args[])
+
+    {
+        int num = 18;
+        long value = 78073453L;
+        double decimal_value = 23.4535767748378;
+        boolean val = true;
+        char character = 'p';
+        System.out.println("Integer value:"+num);
+        System.out.println("Long value:"+value);
+        System.out.println("Double value:"+decimal_value);
+        System.out.println("Boolean value:"+val);
+        System.out.println("Character value:"+character);
+    }
+}
