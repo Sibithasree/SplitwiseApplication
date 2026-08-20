ecb3441ae4434babd32b566efbe3a81506f1ddda
Sibima <sibithasree.25bscaids@kprcas.ac.in>
2026-08-20 20:28:56 +0530

    4th commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    void greet() {
-        System.out.println("Hello world");
-    }
+public static void main(String args[])
 
-    public static void main(String[] args) {
-        Main obj = new Main();
-        obj.greet();
-    }
+{
+    double value=9.7;
+    int num=(int)value;
+    int round=(int)Math.round(value);
+    System.out.println("Integer value:"+num);
+    System.out.println("Rounded value:"+round);
+    int value1=5;
+    int value2=2;
+    int result=5/2;//dividing both numbers and storing result as integer
+    double res=5/2;//dividing both numbers and storing result as double
+    System.out.println("Result when stored as Integer:"+result);
+    System.out.println("Result when stored as double:"+res);
+}
 }
