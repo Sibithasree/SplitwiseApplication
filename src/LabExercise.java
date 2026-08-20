d3b60564b748c8595d10dd065d9a295e268060e1
Sibima <sibithasree.25bscaids@kprcas.ac.in>
2026-08-20 20:32:03 +0530

    8th commit

--- src/LabExercise.java
+++ src/LabExercise.java
@@ -1,17 +1,28 @@
 import java.awt.*;
+import java.util.Scanner;
 
 class LabExcercise{
     public static void main(String args[]){
-        int quotient_in_integer=17/5;
-        System.out.println("Result of two integer division:"+quotient_in_integer);
-        double quotient_in_double=17.0/5;
-        System.out.println("Result of a double value with a integer value:"+quotient_in_double);
-        int modulus=17%5;
-        System.out.println("Modulus Result:"+modulus);
         int choice=2;
         System.out.println(choice==2);
         System.out.println(choice!=3);
         System.out.println(choice>=1&&choice<=3);
 
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter your choice:");
+        int choice= scanner.nextInt();
+        if(choice==1){
+            System.out.println("Add");
+        }
+        else if(choice==2)
+        {
+            System.out.println("View");
+        }
+        else if(choice==3){
+            System.out.println("Exit");
+        }
+        else{
+            System.out.println("Invalid");
+        }
     }
 }
