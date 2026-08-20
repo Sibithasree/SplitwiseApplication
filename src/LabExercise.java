c72af3c6289ae0d614c0a192cf8036e44b05c9cb
Sibima <sibithasree.25bscaids@kprcas.ac.in>
2026-08-20 20:31:26 +0530

    7th commit

--- src/LabExercise.java
+++ src/LabExercise.java
@@ -1,20 +1,17 @@
-public class LabExercise {
-    public static void main(String args[])
-    class LabExcercise{
-        public static void main(String args[]){
-            int quotient_in_integer=17/5;
-            System.out.println("Result of two integer division:"+quotient_in_integer);
-            double quotient_in_double=17.0/5;
-            System.out.println("Result of a double value with a integer value:"+quotient_in_double);
-            int modulus=17%5;
-            System.out.println("Modulus Result:"+modulus);
+import java.awt.*;
 
-            {
-                int value1=5;
-                int value2=2;
-                int result=5/2;//dividing both numbers and storing result as integer
-                double res=5/2;//dividing both numbers and storing result as double
-                System.out.println("Result when stored as Integer:"+result);
-                System.out.println("Result when stored as double:"+res);
-            }
-        }
+class LabExcercise{
+    public static void main(String args[]){
+        int quotient_in_integer=17/5;
+        System.out.println("Result of two integer division:"+quotient_in_integer);
+        double quotient_in_double=17.0/5;
+        System.out.println("Result of a double value with a integer value:"+quotient_in_double);
+        int modulus=17%5;
+        System.out.println("Modulus Result:"+modulus);
+        int choice=2;
+        System.out.println(choice==2);
+        System.out.println(choice!=3);
+        System.out.println(choice>=1&&choice<=3);
+
+    }
+}
