11fc4ce8fa41618de528510fbec14985f9305aed
Sibima <sibithasree.25bscaids@kprcas.ac.in>
2026-08-20 20:37:31 +0530

    12th commit

--- src/LabExercise.java
+++ src/LabExercise.java
@@ -1,32 +1,12 @@
-import java.util.Scanner;
-
-
 class LabExcercise{
     public static void main(String args[]){
-        Scanner scanner=new Scanner(System.in);
-        final int add=1;
-        final int view=2;
-        final int exit=3;
-        System.out.println("enter the choice:");
-        int choice=scanner.nextInt();
-        switch(choice){
-            case add->{
-                System.out.println("Add");
-            }
-            case view-> {
-
-                System.out.println("View");
-            }
-            case exit-> {
-
-
-                System.out.println("Exit");
-            }
-            default->
-                    System.out.println("Invalid");
-            int num=5;
-            for(int i=1;i<=num;i++){
+        int num=5;
+        for(int i=1;i<=num;i++){
+            int i=5;
+            while(i>=1){
                 System.out.println(i);
+                i--;
+
             }
         }
     }
